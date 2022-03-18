e810c97d6ae0d30e9ebc0d2ac14650ff4bbbf032
DanielNb10 <dkolevski09@gmail.com>
2022-03-18 20:23:39 +0100

    homework2

---
+++ HomeworkTwo/Exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dec1 = 4.2D;
+            double dec2 = 2.2D;
+
+            double divide1 = dec1 / dec2;
+
+            int num1 = 10;
+            int num2 = 5;
+
+            int divide2 = num1 / num2;
+
+            Console.WriteLine("The result of divide1 is " + divide1);
+            Console.WriteLine("The result of divide2 is " + divide2);
+        }
+    }
+}
---
+++ HomeworkTwo/Exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalCredits = 102;
+            int creditPerMessage = 5;
+
+            int numberOfMessage = totalCredits / creditPerMessage;
+
+            Console.WriteLine("The number of message is " + numberOfMessage);
+        }
+    }
+}
