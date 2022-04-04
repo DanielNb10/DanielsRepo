9bd01781843bef7f797dc53a4faaed122eb6381f
DanielNb10 <dkolevski09@gmail.com>
2022-04-04 02:02:08 +0200

    homework4

---
+++ HomeworkFour/StudentGroup/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[] {"Zdravko", "Petko", "Stanko", "Branko", "Trajko"};
+            string[] studentsG2 = new string[] {"Petar", "Ile", "Darko", "Teo", "Mile"};
+            int number;
+
+            while (true)
+            { 
+                 Console.WriteLine("Vnesi broj izmegu, 1 ili 2: ");
+                 string input = Console.ReadLine();
+                 bool succes = int.TryParse(input, out  number);
+
+            if (!succes)
+              {
+                 Console.WriteLine("Ne vnesovte broj");
+                    continue;
+              }
+                break;
+
+            }
+            if(number == 1)
+            {
+                Console.WriteLine("The students in G1 are :");
+                foreach(string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+
+                }         
+            }
+             else if(number == 2)
+            {
+                Console.WriteLine("The students in G2 are : ");
+                foreach(string student in studentsG2) 
+                {
+                    Console.WriteLine(student);
+                 
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Ne vnesovte broj izmegu 1 i 2");
+            }
+        }
+    }
+}
---
+++ HomeworkFour/SumOfEven/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = new int[6];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Vnesete go " + (i + 1) + " broj");
+                    string input = Console.ReadLine();
+
+                    bool succes = int.TryParse(input, out int number);
+
+                    if (!succes)
+                    {
+                        Console.WriteLine("Vnesete validen broj");
+                        continue;
+                    }
+
+                    numbers[i] = number;
+                    break;
+                }
+            }
+            int sum = 0;
+            foreach(int number in numbers)
+            {
+                if(number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+            Console.WriteLine("Rezultatot e " + sum);
+        }
+    }
+}
