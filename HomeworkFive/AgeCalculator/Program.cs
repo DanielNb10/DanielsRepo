74cd722e8a2e9e968feff694eb9cc3c3f360670d
DanielNb10 <dkolevski09@gmail.com>
2022-04-05 03:33:56 +0200

    homework5

---
+++ HomeworkFive/AgeCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime birthDay = new DateTime(1997, 4, 6);
+            int result = AgeCalculator(birthDay);
+            Console.WriteLine($"You have {result} years.");
+        }
+
+        public static int AgeCalculator(DateTime birthDate)
+        {
+            DateTime now = DateTime.Now;
+            int age = now.Year - birthDate.Year;
+
+            if (now.Month < birthDate.Month || (now.Month == birthDate.Month && now.Day < birthDate.Day))
+                age--;
+
+            return age;
+        }
+    }
+}
---
+++ HomeworkFive/SubString/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SubString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vnesete broj: ");
+            string input = Console.ReadLine();
+
+            bool succes = int.TryParse(input, out int n);
+
+            while(true)
+            { 
+               if (!succes)
+               {
+                  Console.WriteLine("Vnesi broj");
+                    continue;
+               }
+                break;
+            }
+            string final;
+            string result = Substrings();
+
+            if(result.Length < n)
+            {
+                final = result.Substring(0, result.Length );
+                Console.WriteLine(final);
+            }
+            else 
+            { 
+                final = result.Substring(0, n);
+                Console.WriteLine(final);
+                Console.WriteLine(final.Length);
+            }
+
+            
+
+        }
+
+        public static string Substrings()
+        {
+            return  "Hello from SEDC Codeacademy 2022";
+            
+        }
+    }
+}
